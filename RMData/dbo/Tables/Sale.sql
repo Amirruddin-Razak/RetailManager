fec433f357bc13183759e47609436c27b59a9167
Amirruddin-Razak <amirruddinabdulrazak@gmail.com>
2022-03-26 18:38:03 +0800

    Add DB Table for Point Of Sale System

---
+++ RMData/dbo/Tables/Sale.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[CashierId] NVARCHAR(128) NOT NULL,
+	[TransactionDate] DATETIME2 NOT NULL,
+	[Subtotal] MONEY NOT NULL,
+	[Tax] MONEY NOT NULL,
+	[Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Sale_User] FOREIGN KEY ([CashierId]) REFERENCES [User]([Id])
+
+)
---
+++ RMData/dbo/Tables/SaleItem.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SaleItem]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[SaleId] INT NOT NULL,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[SalePrice] MONEY NOT NULL,
+	[Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleItem_Sale] FOREIGN KEY ([SaleId]) REFERENCES [Sale]([Id]), 
+    CONSTRAINT [FK_SaleItem_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
+)
